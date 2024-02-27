892118600123060fc318a0f099b0d9cff64c5e96
Sabohatfrontend <ssobirova333@gmail.com>
2024-02-27 06:51:18 +0500

    feat: add solutions

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+
+  push(element) {
+    this.items[this.length] = element;
+    this.length++;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.length === 0) return undefined;
+    this.length--;
+    let delElement = this.items[this.length];
+    return delElement;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.items[this.length - 1];
   }
 }
 
